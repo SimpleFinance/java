15ac44fdf05fe6711aade3c73f52cbfc40057bb3
Miah Johnson <miah@chia-pet.org>
2013-12-10 15:12:10 -0800

    clean up

--- test/cookbooks/java_test/recipes/openjdk.rb
+++ test/cookbooks/java_test/recipes/openjdk.rb
@@ -1,10 +1,10 @@
 
 java 'openjdk' do
   install_type :package
-  install_options {
+  install_options({
     provider: :openjdk,
-        version: 7,
-        update: 25,
-        release: '2.3.10-1'
-  }
+    version: 7,
+    update: 25,
+    release: '2.3.10-1'
+  })
 end
