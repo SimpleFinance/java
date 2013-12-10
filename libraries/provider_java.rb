c36a3e3fedd6d65b60e8e1218fbc137f7169d10a
Miah Johnson <miah@chia-pet.org>
2013-12-10 12:47:22 -0800

    The Provider provides.

--- libraries/provider_java.rb
+++ libraries/provider_java.rb
@@ -1,4 +1,6 @@
 require 'chef/provider'
+require_relative('instance_tar')
+require_relative('instance_package')
 
 class Chef
   class Provider
@@ -6,6 +8,8 @@ class Chef
 
       def initialize(new_resource, run_context)
         super
+        @install_type = new_resource.install_type
+        @install_options = new_resource.install_options
       end
 
       def whyrun_supported?
@@ -16,19 +20,31 @@ class Chef
       end
 
       def action_create
+        instance(@new_resource.install_type, :install)
+        @new_resource.updated_by_last_action(true)
       end
 
       def action_destroy
+        instance(@new_resource.install_type, :remove)
+        @new_resource.updated_by_last_action(true)
       end
 
       private
 
-      def perform_action
+      def instance(type, action)
+        instance_class = instance_sub_class(type)
+        i = instance_class.new(@new_resource, @run_context)
+        i.send(action)
       end
 
-      def installer
+      def instance_sub_class(type)
+        klass = "ChefJava::Instance::#{ type_to_sc(type) }"
+        klass.split('::').reduce(Object) { |kls, t| kls.const_get(t) }
       end
 
+      def type_to_sc(type)
+        type.to_s.capitalize
+      end
     end
   end
 end
