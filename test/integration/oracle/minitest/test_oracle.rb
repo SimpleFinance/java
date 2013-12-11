5567ead92e6497397e8a3b200dd923d0eaf2c159
Miah Johnson <miah@chia-pet.org>
2013-12-10 22:55:47 -0800

    add messages to the tests.

--- test/integration/oracle/minitest/test_oracle.rb
+++ test/integration/oracle/minitest/test_oracle.rb
@@ -3,19 +3,19 @@ require 'minitest/autorun'
 describe 'java_test::oracle' do
 
   it 'should have /tmp/java.tar.gz' do
-    assert File.exists?('/tmp/java.tar.gz')
+    assert(File.exists?('/tmp/java.tar.gz'), 'Java Tarball missing.')
   end
 
   it 'should extract java to /opt' do
-    assert File.directory?('/opt/java')
+    assert(File.directory?('/opt/java'), '/opt/java doesnt exist.')
   end
 
   it 'should create a java binary' do
-    assert File.exists?('/opt/java/bin/java')
+    assert(File.exists?('/opt/java/bin/java'), '/opt/java/bin/java is missing.')
   end
 
   it 'should create a symlink' do
-    assert File.symlink?('/opt/java/sbin')
+    assert(File.symlink?('/opt/java/sbin'), '/opt/java/sbin is not a symlink.')
   end
 
 end
