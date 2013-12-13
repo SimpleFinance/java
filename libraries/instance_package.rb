6113ff9775854b96e1637863ac65fdd4fee1d24a
Miah Johnson <miah@chia-pet.org>
2013-12-12 20:01:30 -0800

    some of this.

--- libraries/instance_package.rb
+++ libraries/instance_package.rb
@@ -1,3 +1,4 @@
+require 'chef/resource/package'
 
 module ChefJava
   module Instance
@@ -7,18 +8,55 @@ module ChefJava
         @options = options
       end
 
-      def self.installed?
-      end
-
-      def self.version
-      end
-
       def install
-        true
       end
 
       def remove
-        true
+      end
+
+      private
+
+      def set_package_resource
+        @package ||= Chef::Resource::Package.new(package_name)
+      end
+
+      def package_name
+        @package_name ||= @options.fetch(:provider)
+      end
+
+      def rendered_package_version
+        @pkg_version ||= "#{ pkg_version }u#{ pkg_update }-#{ pkg_release }"
+      end
+
+      def pkg_version
+        @options.fetch(:version)
+      end
+
+      def pkg_update
+        @options.fetch(:update)
+      end
+
+      def pkg_release
+        @options.fetch(:release)
+      end
+
+      def pkg_source
+        @options.fetch(:source)
+      end
+
+      def pkg_options
+        @options.fetch(:options)
+      end
+
+      def pkg_response_file
+        @options.fetch(:response_File)
+      end
+
+      def package_resource(res, action)
+        res.version rendered_package_version
+        res.source if pkg_source
+        res.options if pkg_options
+        res.action action
       end
     end
   end
