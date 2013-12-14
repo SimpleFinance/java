34070907317d910916645d3a04bf271b01381408
Miah Johnson <miah@chia-pet.org>
2013-12-13 16:24:49 -0800

    call extract_jce

--- libraries/instance_tar.rb
+++ libraries/instance_tar.rb
@@ -3,6 +3,7 @@
 # Extract Java to destination directory
 
 require_relative 'tar_helper'
+require_relative 'zip_helper'
 
 module ChefJava
   module Instance
@@ -14,6 +15,7 @@ module ChefJava
 
       def install
         extract_tar
+        extract_jce
       end
 
       def remove
@@ -30,10 +32,18 @@ module ChefJava
         @options.fetch(:destination) { :tar_destination_unspecified }
       end
 
-      #
+      def install_jce?
+        @options.fetch(:install_jce) { false }
+      end
+
       def extract_tar
         tar = ChefJava::Helpers::Tar.new(archive, destination)
         tar.extract_tar
+      end
+
+      def extract_jce
+        jce = ChefJava::Helpers::Zip.new(archive, destination)
+        jce.extract_zip
       end
 
       def remove_extracted_tar
