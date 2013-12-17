1b827e4d47705515cdce159cabf98f50e67b429c
Miah Johnson <miah@chia-pet.org>
2013-12-16 19:39:11 -0800

    jce_helper. Call upon the zip_helper to get lists of files and extract them to a place.

---
+++ libraries/jce_helper.rb
@@ -0,0 +1,50 @@
+require_relative 'zip_helper'
+
+module ChefJava
+  module Helpers
+    class Jce
+      def initialize(archive, destination, run_context)
+        @archive = archive
+        @destination = destination
+        @run_context = run_context
+      end
+
+      def extract
+        jce_files.each do |file|
+          zip_handle.extract(file, jce_destination(file))
+        end
+      end
+
+      private
+
+      def zip
+        @zip ||= ChefJava::Helpers::Zip.new(@archive,
+                                            jce_path,
+                                            @run_context)
+      end
+
+      def zip_handle
+        zip.safe_handle
+      end
+
+      def jce_files
+        open_zip = zip_handle
+        files = open_zip.collect { |entry| entry.ftype == :file ? entry : nil }
+        files.compact!
+        files
+      end
+
+      def jce_path
+        File.join(@destination, 'lib', 'security')
+      end
+
+      def jce_file_name(entry)
+        entry.name.split('/').last
+      end
+
+      def jce_destination(file)
+        File.join(jce_path, jce_file_name(file))
+      end
+    end
+  end
+end
