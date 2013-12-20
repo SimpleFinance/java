9fa524d681a8d153a01c4a12d510f5580bcd1fca
Miah Johnson <miah@chia-pet.org>
2013-12-19 18:55:22 -0800

    alternatives resource provider

---
+++ libraries/resource_alternatives.rb
@@ -0,0 +1,63 @@
+#
+# Cookbook Name:: java
+# Resource:: alternatives
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+require 'chef/resource'
+
+class Chef
+  class Resource
+
+    class JavaAlternatives < Chef::Resource
+      # we have to set default for the supports attribute
+      # in initializer since it is a 'reserved' attribute name
+      def initialize(*args)
+        super
+        @action = :set
+        @allowed_actions = [:set, :unset]
+      end
+
+
+      def java_location(arg = nil)
+        set_or_return(:java_location,
+                      arg,
+                      kind_of: String,
+                      default: nil)
+      end
+
+      def bin_cmds(arg = nil)
+        set_or_return(:bin_cmds,
+                      arg,
+                      kind_of: Array,
+                      default: nil)
+      end
+
+      def default(arg = nil)
+        set_or_return(:default,
+                      arg,
+                      kind_of: [TrueClass, FalseClass],
+                      default: true)
+      end
+
+      def priority(arg = nil)
+        set_or_return(:priority,
+                      arg,
+                      kind_of: Fixnum,
+                      default: 1061)
+      end
+
+    end
+  end
+end
+
