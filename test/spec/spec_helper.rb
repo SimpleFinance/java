7c6dbfa2a7614095bf288a36dc85c06fd13d9a37
Miah Johnson <miah@chia-pet.org>
2013-12-24 10:39:34 -0800

    spec_helper

---
+++ test/spec/spec_helper.rb
@@ -0,0 +1,11 @@
+require 'coveralls'
+Coveralls.wear!
+
+require 'chef/event_dispatch/dispatcher'
+require 'chef/platform'
+require 'chef/run_context'
+require 'chef/resource'
+require 'chef/resource/service'
+require 'chef/provider'
+require 'minitest/autorun'
+require 'minitest/pride'
