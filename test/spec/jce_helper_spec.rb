62341feaf584a2da3ecdfc261f252f512e63b9a7
Miah Johnson <miah@chia-pet.org>
2013-12-29 23:17:15 -0800

    fix should references.

--- test/spec/jce_helper_spec.rb
+++ test/spec/jce_helper_spec.rb
@@ -4,7 +4,7 @@ describe 'ChefJava::Helpers::Jce' do
   describe 'extract' do
 
     it 'should Extract JCE from zip to destination.' do
-      true.should == false
+      skip
     end
 
   end
--- test/spec/tar_helper_spec.rb
+++ test/spec/tar_helper_spec.rb
@@ -4,7 +4,7 @@ describe 'ChefJava::Helpers::Tar' do
   describe 'extract_tar' do
 
     it 'should Extract tape archive to destination.' do
-      true.should == false
+      skip
     end
 
   end
